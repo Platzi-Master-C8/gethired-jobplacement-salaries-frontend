cebcfb19a423eaff54f73dddd45e8ce12de9b03e
johannpereze <sebas.jsp@gmail.com>
2021-11-28 18:42:35 -0500

    feat: added normaldistributionChart component temporarily to app.js to check changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { THEME } from 'Constants/theme.constant';
 
 import Routes from 'Routes';
 
+import { NormalDistributionChart } from './components/charts/NormalDistributionChart';
+
 import 'Styles/style.scss';
 
 const App = () => (
     <React.StrictMode>
         <ThemeProvider theme={THEME}>
             <Routes />
+            <NormalDistributionChart />
         </ThemeProvider>
     </React.StrictMode>
 );
