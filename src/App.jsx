ec8680908e0ebd81105f0d443c306ba82896b76c
johannpereze <sebas.jsp@gmail.com>
2021-11-28 12:39:17 -0500

    chore: created file for NormalDitributionChart component and added it to the app.js to render

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import { THEME } from 'Constants/theme.constant';
 import Routes from 'Routes';
 
 import 'Styles/style.scss';
+import { NormalDistributionChart } from './components/charts/NormalDistributionChart';
 
 const App = () => (
     <React.StrictMode>
         <ThemeProvider theme={THEME}>
             <Routes />
+            <NormalDistributionChart />
         </ThemeProvider>
     </React.StrictMode>
 );
---
+++ src/components/charts/NormalDistributionChart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NormalDistributionChart = () => {
+    return (
+        <div>
+            <br />
+            <br />
+            <hr />
+            <h3>Normal Distribution Chart (WIP - Johann)</h3>
+        </div>
+    );
+};
