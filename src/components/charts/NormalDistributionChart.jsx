1ad4d66a334ae632df923d64f276e9c0a2f7bb8a
johannpereze <sebas.jsp@gmail.com>
2021-11-28 18:16:39 -0500

    feat: added chart.js library and created firs chart component this new component is called NormalDistributionChart that is rendering with faked data. still WIP

--- src/components/charts/NormalDistributionChart.jsx
+++ src/components/charts/NormalDistributionChart.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { COLORS } from '@master-c8/theme';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Normal distribution of salaries',
+        },
+    },
+};
+
+const labels = ['10%', '25%', '50% (median)', '75%', '90%'];
+const salaries1 = [10, 25, 50, 25, 10];
+const salaries2 = [8, 23, 48, 23, 8];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Profile 1',
+            data: labels.map((label, i) => salaries1[i]),
+            borderColor: COLORS.contrast1,
+            borderWidth: 4,
+            tension: 0.4,
+        },
+        {
+            label: 'Profile 2',
+            data: labels.map((label, i) => salaries2[i]),
+            borderColor: COLORS.secondary,
+            borderWidth: 4,
+            tension: 0.4,
+        },
+    ],
+};
 
 export const NormalDistributionChart = () => {
-    return (
-        <div>
-            <br />
-            <br />
-            <hr />
-            <h3>Normal Distribution Chart (WIP - Johann)</h3>
-        </div>
-    );
+    return <Line options={options} data={data} />;
 };
