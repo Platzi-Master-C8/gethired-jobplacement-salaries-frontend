7c7796c193ffd0c8fae8a1486ed3e4a0582da3a8
EmlSn <eml.snzrmz@gmail.com>
2021-12-09 11:57:51 -0600

    feat: calendar component initial config
    Co-authored-by: Kev Farid <KevFarid@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ import { THEME } from 'Constants/theme.constant';
 import Routes from 'Routes';
 
 import 'Styles/style.scss';
-import { NormalDistributionChart } from './components/charts/NormalDistributionChart';
 
 const App = () => (
     <React.StrictMode>
         <ThemeProvider theme={THEME}>
             <Routes />
-            <NormalDistributionChart />
         </ThemeProvider>
     </React.StrictMode>
 );
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,31 +1,98 @@
 import React from 'react';
+// import { BriefCase } from '@master-c8/icons';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import TimePicker from '@mui/lab/TimePicker';
+import Box from '@mui/material/Box';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
+import MenuItem from '@mui/material/MenuItem';
+
+const platformOptions = [
+    {
+        value: 'Zoom',
+        label: 'Zoom',
+    },
+    {
+        value: 'Google Meet',
+        label: 'Google Meet',
+    },
+    {
+        value: 'Microsoft Teams',
+        label: 'Microsoft Teams',
+    },
+    {
+        value: 'Slack',
+        label: 'Slack',
+    },
+];
 
 const Calendar = () => {
-    const [value, setValue] = React.useState(null);
+    const [value, setValue] = React.useState(new Date());
+
+    const [platform, setPlatform] = React.useState('');
+
+    const handleChange = (event) => {
+        setPlatform(event.target.value);
+    };
 
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DatePicker
-                label="Basic example"
-                value={value}
-                onChange={(newValue) => {
-                    setValue(newValue);
+            <Box
+                sx={{
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: '#F8F8F8',
+                    // '&:hover': {
+                    //     backgroundColor: '#F8F8F8',
+                    // opacity: [0.9, 0.8, 0.7],
+                    // },
                 }}
-                renderInput={(params) => <TextField {...params} />}
-            />
-            <TimePicker
-                label="Basic example"
-                value={value}
-                onChange={(newValue) => {
-                    setValue(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-            />
+            >
+                <StaticDatePicker
+                    displayStaticWrapperAs="desktop"
+                    openTo="day"
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                <TextField
+                    id="filled-select-platform"
+                    select
+                    label="Select Platform"
+                    value={platform}
+                    onChange={handleChange}
+                    helperText="Select a platform"
+                    variant="filled"
+                >
+                    {platformOptions.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <DatePicker
+                    label="Date"
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                <TimePicker
+                    label="Time"
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                <TextField id="filled-basic" label="URL" type="url" variant="filled" />
+                <TextField id="filled-basic" label="Phone Number" type="tel" variant="filled" />
+            </Box>
         </LocalizationProvider>
     );
 };
