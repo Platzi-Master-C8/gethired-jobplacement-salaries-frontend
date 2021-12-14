a617aa86034c02aad6f28bb0a14760325d843365
Yadurani Lopez <yadu-lopez@hotmail.com>
2021-12-14 14:06:36 -0500

    fix: add provider for tests

--- src/__tests__/FormCard.test.jsx
+++ src/__tests__/FormCard.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 
 import FormCard from 'Components/FormCard';
+import ProviderMock from '../__mocks__/Providermock';
 
 const initialValues = {
     jobTitle: '',
@@ -10,23 +11,28 @@ const initialValues = {
     englishLevel: '',
 };
 
-describe('Tests for component <FormCard/>', () => {
+const ComponentForm = (props) => {
     const handleChange = jest.fn();
+    const handleDelete = jest.fn();
+    return (
+        <ProviderMock>
+            <FormCard values={initialValues} onChange={handleChange} onDelete={handleDelete} {...props} />
+        </ProviderMock>
+    );
+};
+
+describe('Tests for component <FormCard/>', () => {
     test('Take Snapshot', () => {
-        const { asFragment } = render(
-            <FormCard title="Calculate Salary" values={initialValues} onChange={handleChange} />,
-        );
+        const { asFragment } = render(<ComponentForm title="Calculate Salary" />);
 
         expect(asFragment()).toMatchSnapshot();
     });
     test('Expect title Calculate Salary for card', async () => {
-        const { getByText } = render(
-            <FormCard title="Calculate Salary" values={initialValues} onChange={handleChange} />,
-        );
+        const { getByText } = render(<ComponentForm title="Calculate Salary" />);
         await waitFor(() => expect(getByText(/Calculate Salary/i)).toBeInTheDocument());
     });
     test('Have 4 selects', () => {
-        const { container } = render(<FormCard values={initialValues} onChange={handleChange} />);
+        const { container } = render(<ComponentForm />);
         expect(container.querySelectorAll('input')).toHaveLength(4);
     });
 });
