5f09463187dd43b410e1b77755f04beb074d59ea
johannpereze <sebas.jsp@gmail.com>
2022-01-18 10:08:11 -0500

    feat: added snackbar component for error message. WIP

--- src/pages/Salaries.jsx
+++ src/pages/Salaries.jsx
@@ -11,6 +11,7 @@ import { HeaderJob } from '@master-c8/commons';
 import { TabPanel } from 'Components/Commons/Tabs';
 import TabCompare from 'Components/TabCompare';
 import TabCalculate from 'Components/TabCalculate';
+import { SnackbarMessage } from '../components/Commons/SnackbarMessage/SnackbarMessage';
 
 const Theme = () => {
     const [tabs, setValue] = useState(0);
@@ -44,6 +45,7 @@ const Theme = () => {
                     </TabPanel>
                 </Box>
             </Container>
+            <SnackbarMessage />
         </Fragment>
     );
 };
