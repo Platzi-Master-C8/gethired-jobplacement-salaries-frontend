b0e699f7b92268aa950203d674d41ea21113debd
johannpereze <sebas.jsp@gmail.com>
2022-01-21 17:52:58 -0500

    feat: the snackbar now activates after http error

--- src/app/CalculateSalary/slice.js
+++ src/app/CalculateSalary/slice.js
@@ -23,12 +23,30 @@ const initialState = {
     snackbarShow: false,
 };
 
-export const fetchChartData = createAsyncThunk('post/fetchChartData', (profile) =>
-    getSalaryProfile('salaries', profile)
+export const fetchChartData = createAsyncThunk('post/fetchChartData', async (profile, { rejectWithValue }) => {
+    const salaryProfile = await getSalaryProfile('salaries', profile)
+
+    if (!salaryProfile.average) {
+        return rejectWithValue(salaryProfile)
+    }
+
+    return salaryProfile
+}
 );
 
-export const fetchComparisonChartData = createAsyncThunk('post/fetComparisonchChartData', async ([profile1, profile2]) =>
-    [await getSalaryProfile('salaries', profile1), await getSalaryProfile('salaries', profile2)]
+export const fetchComparisonChartData = createAsyncThunk('post/fetComparisonchChartData', async ([profile1, profile2], { rejectWithValue }) => {
+    const salaryProfile1 = await getSalaryProfile('salaries', profile1)
+    const salaryProfile2 = await getSalaryProfile('salaries', profile2)
+
+    if (!salaryProfile1.average) {
+        return rejectWithValue(salaryProfile1)
+    }
+    if (!salaryProfile2.average) {
+        return rejectWithValue(salaryProfile2)
+    }
+
+    return [salaryProfile1, salaryProfile2]
+}
 );
 
 const calculateSalary = createSlice({
@@ -59,15 +77,25 @@ const calculateSalary = createSlice({
     },
     extraReducers: {
         [fetchChartData.fulfilled]: (state, action) => {
+            console.log('fetchChartData.fulfilled');
             state.chartData = [action.payload];
         },
+        [fetchChartData.pending]: (state) => {
+            console.log('fetchChartData.pending');
+        },
         [fetchChartData.rejected]: (state) => {
+            console.log('fetchChartData.rejected');
             state.snackbarShow = true;
         },
         [fetchComparisonChartData.fulfilled]: (state, action) => {
+            console.log('fetchComparisonChartData.fulfilled');
             state.comparisonChartData = action.payload;
         },
-        [fetchComparisonChartData.fulfilled]: (state) => {
+        [fetchComparisonChartData.pending]: (state, action) => {
+            console.log('fetchComparisonChartData.pending');
+        },
+        [fetchComparisonChartData.rejected]: (state) => {
+            console.log('fetchComparisonChartData.rejected');
             state.snackbarShow = true;
         },
     },
